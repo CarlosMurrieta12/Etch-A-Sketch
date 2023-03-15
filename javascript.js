a559afcedda18b7f0b67ca3b93193caed7a1e997
CarlosMurrieta12 <emilimurrieta@icloud.com>
2023-03-14 18:13:00 -0700

    Added the hover effect to the grid

--- javascript.js
+++ javascript.js
@@ -1,8 +1,21 @@
 const container = document.querySelector('.container');
 
+// Grid
+
 for (let i = 0; i < 256; i++) {
     const board = document.createElement('div');
     board.classList = 'grid';
 
     container.appendChild(board);
 };
+
+// "Hover" effect
+
+let newColor = document.querySelectorAll(".grid");
+for (const color of newColor) {
+    color.addEventListener("mouseover", () => {
+        color.style.backgroundColor = "black";
+    })
+}
+
+// Button to change grid size
